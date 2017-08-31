cf231f457b8aa0602f720811ac3a8aca5f2fbc8f
ENZO <ENZO@ENZO-HOME>
2017-08-31 12:11:09 +0800

    Added MainEntryPoint in OmicronLib Added Setup and Loop

--- Omicron9/OmicronLib/src/main/java/org/xyzeelab/omicronlib/MainEntryPoint.java
+++ Omicron9/OmicronLib/src/main/java/org/xyzeelab/omicronlib/MainEntryPoint.java
@@ -10,8 +10,26 @@ package org.xyzeelab.omicronlib;
  * @author ENZO
  */
 public class MainEntryPoint {
+    static boolean loop = true;
+    
     public static void main(String args[]) {
         // Entry point for the system do all the initialization here. 
+        
+        // Call the setup to routine. 
+        Setup();
+        
+        // Start looping.
+        while(loop) {
+           Loop();
+        }
+        
+    }
+    
+    public static void Setup() {
+        
+    }
+    
+    public static void Loop() {
         
     }
 }
