4b82adfeebcbb1d227a2e1fbfb5d49518d7d63d4
ENZO <ENZO@ENZO-HOME>
2017-08-31 16:32:09 +0800

    Added MainEntryPoint in OmicronLib Added Setup and Loop Added LED blink

--- Omicron9/OmicronLib/src/main/java/org/xyzeelab/omicronlib/MainEntryPoint.java
+++ Omicron9/OmicronLib/src/main/java/org/xyzeelab/omicronlib/MainEntryPoint.java
@@ -5,12 +5,19 @@
  */
 package org.xyzeelab.omicronlib;
 
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
 /**
  *
  * @author ENZO
  */
 public class MainEntryPoint {
     static boolean loop = true;
+    static GpioController gpio;
     
     public static void main(String args[]) {
         // Entry point for the system do all the initialization here. 
@@ -26,10 +33,11 @@ public class MainEntryPoint {
     }
     
     public static void Setup() {
-        
+        gpio = GpioFactory.getInstance();
     }
     
     public static void Loop() {
-        
+        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+        pin.blink(200);
     }
 }
